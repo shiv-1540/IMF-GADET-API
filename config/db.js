d4edc11b3cb4a3cd089e9e5807a49c8a2f68b1fd
Shiv Ghyar <shivghyar538@gmail.com>
2025-02-23 05:19:17 +0530

    changing dbb rejectunauthorrized issue

--- config/db.js
+++ config/db.js
@@ -1,9 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'postgres',
-  logging: false,
-});
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+  
 
 module.exports = sequelize;
